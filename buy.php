4655bf8a40cb460896ebd92f4b8b192b7ffd041a
Alyssa <akmorrow@wisc.edu>
2015-07-18 07:27:03 -0700

    paypal works

---
+++ buy.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>About</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+     <!-- Custom styles for this template -->
+    <link href="../css/navbar.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../css/style.css" rel="stylesheet">
+
+    <script type="text/javascript" src="../js/paypal-button.min.js"></script>
+    <!-- Custom Fonts -->
+    <link href="../font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+
+<body>
+
+<h1>BUY</h1>
+
+<form name="_xclick" action="https://www.paypal.com/us/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick" />
+<input type="hidden" name="business" value="morrowalyssa@gmail.com" />
+<input type="hidden" name="currency_code" value="USD" />
+<input type="hidden" name="item_name" value="Hope Bars" />
+<input type="hidden" name="amount" value="24" placeholder="Number of Boxes" />
+<input type="text" name="quantity" value="" placeholder="Number of Boxes" />
+<input type="image" src="http://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!" />
+</form>
+
+
+   
+<!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <br>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="https://www.facebook.com/groups/HOPEBARS/"><i class="fa fa-facebook-square fa-fw fa-3x"></i></a>
+                            <!-- insert facebook pic-->
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/em2dproject"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+                        </li>
+                    </ul>
+                    <hr class="small">
+                    <p class="text-muted">Copyright &copy; MARSS 2015</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+  
+
+</body>
+
+
+
+</html>
+
